a393de63b1dc5ebbb43dc6c36af020c0cfbe94e4
DaLiu <liuxing8807@126.com>
2020-06-12 19:17:33 +0800

    Change Public file

--- BMAutoLayout/BMAutoLayout/BMAutoLayout.h
+++ BMAutoLayout/BMAutoLayout/BMAutoLayout.h
@@ -9,13 +9,13 @@ FOUNDATION_EXPORT const unsigned char BMAutoLayoutVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <BMAutoLayout/PublicHeader.h>
 
-#if __has_include(<BMModel/BMModelLib.h>)
-#   import <BMModel/BMUtilities.h>
-#   import <BMModel/UIView+BMAdditions.h>
-#   import <BMModel/NSArray+BMAdditions.h>
-#   import <BMModel/BMConstraint.h>
-#   import <BMModel/BMCompositeConstraint.h>
-#   import <BMModel/BMConstraintMaker.h>
+#if __has_include(<BMAutoLayout/BMAutoLayout.h>)
+#   import <BMAutoLayout/BMUtilities.h>
+#   import <BMAutoLayout/UIView+BMAdditions.h>
+#   import <BMAutoLayout/NSArray+BMAdditions.h>
+#   import <BMAutoLayout/BMConstraint.h>
+#   import <BMAutoLayout/BMCompositeConstraint.h>
+#   import <BMAutoLayout/BMConstraintMaker.h>
 #else
 #   import "BMUtilities.h"
 #   import "UIView+BMAdditions.h"
